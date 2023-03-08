c3e1db2a96d8d429265e7bc3d991eb9e469d5ea7
Cedric Mercado <cedricpm12@gmail.com>
2023-03-08 08:42:16 -0500

    added some comments to understand some of the code better

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ from pygame.locals import *
 
 # 3/08/23 - goal is to make a user-controlled object
 
-pygame.init()
-screen = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Ced Hero')
+# Colors     R    G    B
+BLACK   =  (  0,   0,   0)
+WHITE   =  (255, 255, 255)
+RED     =  (255,   0,   0)
+GREEN   =  (  0, 255,   0)
+BLUE    =  (  0,   0, 255)
+SKYBLUE =  (  3, 248, 252)
 
+pygame.init()
+screen = pygame.display.set_mode((400, 300))  # Window size
+screen.fill(WHITE)  # Screen color
+pygame.display.set_caption('Ced Game')  # Window title
+
+# Drawing
+pygame.draw.rect(screen, GREEN, (0, 250, 400, 100))
 
 # Main Game Loop
 while True:
-    for event in pygame.event.get():
+    for event in pygame.event.get(): # Event handler
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
