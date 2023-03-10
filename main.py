febd51451884cf267fd0fa2560962b459bea187d
Speed652 <102246128+Speed652@users.noreply.github.com>
2023-03-09 21:52:01 -0500

    Update main.py
    Changed game name to The Epic Adventures of Cedybedy, changed window size numbers to variables so I could use the variables to create a border so the character cant leave the screen

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 SKYBLUE = (3, 248, 252)
 
+# Window size
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 800
+
 
 # Drawing
 # pygame.draw.rect(screen, GREEN, (0, 250, 400, 100))
@@ -23,8 +27,8 @@ def main():
     # Initiate PyGame
     pygame.init()
     # Window size
-    display = pygame.display.set_mode((400, 300))
-    pygame.display.set_caption('Ced Game')  # Window title
+    display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('The Epic Adventures of Cedybedy')  # Window title
     clock = pygame.time.Clock()
     display.fill(WHITE)  # Screen color
 
@@ -39,6 +43,15 @@ def main():
                 sys.exit()
 
         character.move(events)
+        
+        if character.pos[0] < 15:
+            character.pos[0] = 15
+        elif character.pos[0] + character.width > WINDOW_WIDTH:
+            character.pos[0] = WINDOW_WIDTH - character.width
+        if character.pos[1] < 15:
+            character.pos[1] = 15
+        elif character.pos[1] + character.height > WINDOW_HEIGHT:
+            character.pos[1] = WINDOW_HEIGHT - character.height
 
         display.fill((255, 255, 255))
         # ^ clears the screen every move
