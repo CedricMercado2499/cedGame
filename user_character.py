0596d1710ea822b30de3a341f21bb17c6c1824de
Cedric Mercado <cedricpm12@gmail.com>
2023-03-15 12:01:38 -0400

    assigned character skin colour for each character

--- user_character.py
+++ user_character.py
@@ -12,6 +12,11 @@ from pygame.sprite import Sprite
 # Implement physics (velocity)
 # Ways to organize code, separate blocks into functions
 
+def open_options():  # Work-In-Progress, Options Menu isn't created, currently goes back to character selection
+    from main import main
+    main()
+
+
 class Player(Sprite):
 
     def __init__(self, pos, user_name):
@@ -25,7 +30,11 @@ class Player(Sprite):
         self.user_name = user_name
 
         if user_name == "Mohamed" or user_name == "Munashe":
-            self.skin = BLACK
+            self.skin = SKIN_BLACK
+        elif user_name == "Cedric" or user_name == "Roberto" or user_name == "Fernando":
+            self.skin = SKIN_ASIAN
+        elif user_name == "Varun":
+            self.skin = SKIN_BROWN
         else:
             self.skin = SKIN_WHITE
 
@@ -47,33 +56,29 @@ class Player(Sprite):
         self.rect.x = self.pos[0]
         self.rect.y = self.pos[1]
 
-    def move(self):
+    # Keybind Movement
+    def update_position(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.pos[0] -= 0.7
-            self.left, self.right, self.up, self.down = True, False, False, False
+            self.move_left()
 
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.pos[0] += 0.7
-            self.left, self.right, self.up, self.down = False, True, False, False
+            self.move_right()
 
         if keys[pygame.K_UP] or keys[pygame.K_w]:
-            self.left, self.right, self.up, self.down = False, False, True, False
-
-        if keys[pygame.K_SPACE]:
-            if self.jumping:
-                self.jump_velocity = -7  # Change this to inc/dec jump
-                self.jumping = False
+            self.look_up()
 
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            self.left, self.right, self.up, self.down = False, False, False, True
+            self.look_down()
+
+        if keys[pygame.K_SPACE]:
+            self.do_jump()
 
         if keys[pygame.K_ESCAPE]:  # Change character
-            from main import main
-            main()
+            open_options()
 
-        # Border
+        # Barrier
         from main import WINDOW_HEIGHT, WINDOW_WIDTH
         if self.pos[0] < 0:
             self.pos[0] = 0
@@ -84,18 +89,41 @@ class Player(Sprite):
         elif self.pos[1] + self.height > WINDOW_HEIGHT:
             self.pos[1] = WINDOW_HEIGHT - self.height
 
+    def apply_gravity(self):
+        from main import WINDOW_HEIGHT, WINDOW_WIDTH
         # Gravity V2
         self.jump_velocity += self.gravity
         self.pos[1] += self.jump_velocity
+        # jump_cd = 3000
+        # jump_timer = pygame.time.get_ticks()
         if self.pos[1] + self.height > WINDOW_HEIGHT:
             self.pos[1] = WINDOW_HEIGHT - self.height
+            # if pygame.time.get_ticks() - jump_timer > jump_cd:
             self.jumping = True
             # if character is on the floor, jump
             # must change when applying platforms
+        # Movement Functions
 
-    # import main.py width
+    def move_left(self):
+        self.pos[0] -= 0.7
+        self.left, self.right, self.up, self.down = True, False, False, False
+
+    def move_right(self):
+        self.pos[0] += 0.7
+        self.left, self.right, self.up, self.down = False, True, False, False
+
+    def look_up(self):
+        self.left, self.right, self.up, self.down = False, False, True, False
+
+    def look_down(self):
+        self.left, self.right, self.up, self.down = False, False, False, True
+
+    def do_jump(self):
+        if self.jumping:
+            self.jump_velocity = -7  # Change this to inc/dec jump
+            self.jumping = False
+
     def draw(self, display):
-
         headPos = (self.rect.x + 5, self.rect.y - 20)
         eyePos = (self.rect.x, self.rect.y)
 
@@ -113,7 +141,6 @@ class Player(Sprite):
         character_rect = character_text.get_rect(
             center=(self.rect.x + character_text.get_width() // 2.5, (self.rect.y - character_text.get_height() * 3)))
 
-
         # Draw Body
         pygame.draw.rect(display, self.color, (*self.pos, self.width, self.height))
         # Draw Head
@@ -124,6 +151,7 @@ class Player(Sprite):
             pygame.draw.rect(display, (100, 100, 100), (*eyePos, self.width - 20, self.height - 20))
 
     def update(self):
-        self.move()
+        self.update_position()
+        self.apply_gravity()
         self.rect.x = self.pos[0]
         self.rect.y = self.pos[1]
